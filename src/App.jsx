ef3cf55a2e5305aa070744203b23d31fdef51baf
Rafael Almeida Penha <rafaelalmeidapenha@gmail.com>
2022-10-18 11:57:51 -0300

    Alterado CSS dos charts, Json de bebidas adicionado na logica.

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import { useState } from "react";
 import "./App.css";
 import Charts from "./components/charts/Charts";
 import Search from "./components/search/Search";
-import { bebidas } from "./constants";
+import bebidas from "./bebidas.json";
 
 function App() {
   const [search, setSearch] = useState("");
 
   return (
     <div className="App">
-      <h1 className="title">Project Dynamic Charts!</h1>
+      <h1 className="Title">Project Dynamic Charts!</h1>
       <Search search={search} setSearch={setSearch} />
       <div className="Container">
-        Charts
+        <div className="Title">Charts</div>
         <div className="Charts">
           {bebidas.map((e, i) => {
             if (e.name.toLowerCase().includes(search.toLowerCase())) {
               return (
-                <Charts key={i} name={e.name} tag={e.tag} price={e.price} />
+                <Charts
+                  key={i}
+                  name={e.name}
+                  tag={e.tag}
+                  price={e.price}
+                  url={e.url}
+                />
               );
             }
+            return this;
           })}
         </div>
       </div>
